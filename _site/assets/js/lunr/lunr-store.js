cb7e2bae3c1bb3db9d7a165d20ea2a2e06dc17d7
mskarha <skidew94@gmail.com>
2018-03-06 20:54:59 -0500

    updated website

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Come play with us!!",
+        "excerpt":"SO YOU WANNA TOSS A PIECE OF PLASTIC? No pig skin, no stick. Not rubber or wood. Only 175g of that sweet ultra star (NEVER WHAMO) brand Frisbee. Well this is the place for you. Come hone your skills with us as we work to be one of the greatest...","categories": ["Layout","Uncategorized"],
+        "tags": ["edge case","featured image","image","layout"],
+        "url": "http://localhost:4000/layout/uncategorized/play/",
+        "teaser":null}]
